e5e0d9699b7b575cda22d71405279d53ab07b1e0
camhickey <cam.s.hickey@gmail.com>
2024-11-30 19:11:05 -0500

    rename project and pull article from db

--- api/index.ts
+++ api/index.ts
@@ -1,48 +1,48 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 const { neon } = require("@neondatabase/serverless");
 
 const sql = neon(process.env.DATABASE_URL);
 
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/:style/:color/:article', async (req, res) => {
+app.get("/:style/:color/:article", async (req, res) => {
   const { style, color, article } = req.params;
-	try {
+  try {
     switch (style) {
       case "urban":
         const urban = await sql`
-          SELECT name, price, image, link
+          SELECT name, price, image, link, article
           FROM urban
           WHERE color = ${color} AND article = ${article}
           ORDER BY RANDOM() LIMIT 1`;
         return res.json(urban);
       case "greyman":
         const greyman = await sql`
-          SELECT name, price, image, link
+          SELECT name, price, image, link, article
           FROM greyman
           WHERE color = ${color} AND article = ${article}
           ORDER BY RANDOM() LIMIT 1`;
         return res.json(greyman);
       case "cyberpunk":
         const cyberpunk = await sql`
-          SELECT name, price, image, link
+          SELECT name, price, image, link, article
           FROM cyberpunk
           WHERE color = ${color} AND article = ${article}
           ORDER BY RANDOM() LIMIT 1`;
         return res.json(cyberpunk);
       case "outdoors":
         const outdoors = await sql`
-          SELECT name, price, image, link
+          SELECT name, price, image, link, article
           FROM outdoors
           WHERE color = ${color} AND article = ${article}
           ORDER BY RANDOM() LIMIT 1`;
@@ -50,13 +50,12 @@ app.get('/:style/:color/:article', async (req, res) => {
       default:
         return res.status(404).json({ error: "Style not found" });
     }
-		}
-	catch (error) {
-		console.error(error);
-		res.status(500).send('Error retrieving clothing');
-	}
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving clothing");
+  }
 });
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
